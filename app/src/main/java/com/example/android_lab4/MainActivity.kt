3de4dcb79cb09273e934f58e82042741e6466d35
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-28 14:02:01 +0700

    Add listener for buttons

--- app/src/main/java/com/example/android_lab4/MainActivity.kt
+++ app/src/main/java/com/example/android_lab4/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.android_lab4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val trueButton = findViewById<Button>(R.id.true_button)
+        val falseButton = findViewById<Button>(R.id.false_button)
+        val nextButton = findViewById<Button>(R.id.next_button)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trueButton.setOnClickListener { _ ->
+
+        }
+
+        falseButton.setOnClickListener { _ ->
+
+        }
+
+        nextButton.setOnClickListener { _ ->
+
+        }
     }
 }
